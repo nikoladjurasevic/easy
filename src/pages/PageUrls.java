fb41ad40a48bba07aca86532e5e421d97fa1c345
nikoladjurasevic <nikola.djurasevic@gmail.com>
2020-04-20 11:39:10 +0200

    adding SingleCheckboxTest

---
+++ src/pages/PageUrls.java
@@ -0,0 +1,7 @@
+package pages;
+
+public final class PageUrls {
+    public static final String homeUrl = "https://www.seleniumeasy.com/test/";
+    public static final String basic_first_form_demo_url = homeUrl + "basic-first-form-demo.html";
+    public static final String basic_checkbox_demo_url = homeUrl + "basic-checkbox-demo.html";
+}
---
+++ src/test/SingleCheckbox.java
@@ -0,0 +1,24 @@
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+import pages.BasePage;
+import pages.PageUrls;
+
+public class SingleCheckbox extends BaseTest {
+
+    @Test
+    public void testSingleCheckboxTest() {
+
+        String sExpectedSuccessMessage = "Success - Check box is checked";
+
+        WebDriver driver = openChromeDriver();
+        BasePage page = new BasePage(driver);
+        page.clickBasicExamples();
+        page.clickChecBoxDemoLinkFromBoard();;
+        String sCurrentUrl = driver.getCurrentUrl();
+        assert sCurrentUrl.contains(PageUrls.basic_checkbox_demo_url) : "Wrong URL. Expected: " + PageUrls.basic_checkbox_demo_url + ", but got: " + sCurrentUrl;
+        page.checkSingleCheckboxDemo();
+        String sCurrentSuccessMessage = page.getSingleCheckboxSuccessMessage();
+        assert sCurrentSuccessMessage.equals(sExpectedSuccessMessage) : "Wrong text: Expected: " + sExpectedSuccessMessage + ", but got: " +sCurrentSuccessMessage;
+        driver.quit();
+    }
+}
